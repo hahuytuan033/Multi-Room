e24d4ce17261b915a74af55b2d227a5226013909
huytuan033 <hahuytuan033@gmail.com>
2024-03-22 15:09:44 +0700

    Update file js

--- index.js
+++ index.js
@@ -4,14 +4,38 @@ const c = canvas.getContext('2d')
 canvas.width = 1024 // 64*16
 canvas.height = 576 //64*9
 
-c.fillStyle = ('white')
-c.fillRect(0, 0, canvas.width, canvas.height) //(x, y, width, height)
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.width = 100
+        this.height = 100
+    }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
 
-let y= 100
+    update() {
+        if (bottom < canvas.height) {
+            y++
+            bottom = y + 100
+        }
+    }
+}
+
+const player = new Player()
+
+
+//let bottom = y + 100
 function animate() {
     window.requestAnimationFrame(animate)
-    c.fillStyle = 'red'
-    c.fillRect(100, y, 100, 100)
-    y++
+    c.fillStyle = ('white')
+    c.fillRect(0, 0, canvas.width, canvas.height) //(x, y, width, height)
+
+    player.draw()
+
 }
 animate()
